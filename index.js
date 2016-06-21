401b195308a6670131b75212213a644ad76f9e8f
Tom Werkheiser <tawerkheiser@gmail.com>
2016-06-20 21:20:43 -0400

    add combine directory

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var MrMix = require('./dist/mrmix.js').MrMix;
 MrMix(function (tasks) {
     // tasks.sass('assets/sass', 'public/css')
     //     .webpack('assets/js/app.js', 'public/js');
-    tasks.combine(['assets/js/1.js', 'assets/js/2.js'], 'public/js', 'vendor.js');
+    tasks.combine('assets/js', 'public/js', 'vendor.js');
 });
--- src/tasks/combine.js
+++ src/tasks/combine.js
@@ -16,15 +16,15 @@ export default class Combine {
     boot() {
 		this.checkDest();
 
+        if ( fs.statSync(this.dest).isDirectory() ) {
+            this.parseDir();
+        }
+
         if ( !Array.isArray(this.src) ) {
             this.src = [this.src];
         }
 
-        if ( fs.statSync(this.dest).isDirectory() ) {
-
-        } else {
-            this.combineFiles();
-        }
+        this.combineFiles();
     }
 
     getFileName() {
@@ -34,11 +34,25 @@ export default class Combine {
     combineFiles() {
         let dirFiles = {};
 
+        this.checkFile();
+
         this.src.map((file) => {
             let reader = fs.readFileSync(file);
 
             fs.appendFileSync(this.getFileName(), reader);
         });
+    }
+
+    parseDir() {
+        let files = [];
+        let filePath = this.src;
+
+        fs.readdirSync(this.src)
+            .forEach((file) => {
+                files.push(`${filePath}/${file}`);
+            });
+
+        this.src = files;
     }
 
     checkDest() {
@@ -48,4 +62,14 @@ export default class Combine {
 			return fs.mkdirSync(this.dest);
 		}
 	}
+
+    checkFile() {
+        let file;
+
+        try {
+            file = fs.statSync(this.getFileName()).isFile();
+
+            fs.unlinkSync(this.getFileName());
+        } catch (e) {}
+    }
 }
