be9c6e69f36b120906be2c4524056971f8eb60e1
Tom Werkheiser <tawerkheiser@gmail.com>
2021-01-13 16:20:28 -0500

    revert prod check

--- src/tasks/webpack.js
+++ src/tasks/webpack.js
@@ -8,7 +8,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpackDevServer = require('webpack-dev-server');
 
 // Internal Dependencies
-const {isDirectory, shouldWatch, shouldHotReload, parseDirectory, isProduction} = require('../helpers/file');
+const {isDirectory, shouldWatch, shouldHotReload, parseDirectory} = require('../helpers/file');
 const {writeHeader, writeLn, writeSpace} = require('../helpers/console');
 const notify = require('../helpers/notifier');
 
@@ -93,7 +93,7 @@ class Webpack {
                     {
                         test: /\.css$/,
                         use: [
-                            !isProduction()
+                            process.env.NODE_ENV !== 'production'
                                 ? 'style-loader'
                                 : MiniCssExtractPlugin.loader,
                             'css-loader'
@@ -102,9 +102,7 @@ class Webpack {
                     {
                         test: /\.s[a|c]ss$/,
                         use: [
-                            !isProduction()
-                                ? 'style-loader'
-                                : MiniCssExtractPlugin.loader,
+                            'style-loader',
                             'css-loader',
                             'sass-loader'
                         ]
